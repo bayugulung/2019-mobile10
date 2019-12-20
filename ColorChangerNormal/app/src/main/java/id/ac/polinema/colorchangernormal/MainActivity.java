5b61949156e98b81b8fd3fa08a1f5cf86b8c7fdc
bayugulung <bayugulung46@gmail.com>
2019-12-20 12:22:24 +0700

    class view model

--- ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
+++ ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import java.util.Random;
@@ -31,4 +32,11 @@ public class MainActivity extends AppCompatActivity {
 		return Color.argb(255,rnd.nextInt(256),rnd.nextInt(256),rnd.nextInt(256));
 	}
 
+	btnChangeColor.setOnClickListener(new View.OnClickListener() {
+		@Override
+		public void onClick(View view) {
+			int color = generateRandomColor();
+			rootView.setBackgroundColor(color);
+		}
+	});
 }
